8e3569b82897116180905789350a778e3131a4af
dira oliobi <odiraoliobi@gmail.com>
2021-01-11 00:10:47 -0300

    updated files

--- js/main.js
+++ js/main.js
@@ -1,21 +1,13 @@
-const showMenu = (toggled, navId) =>{
-    const toggle = document(toggleId),
-    nav = document.getElementById(navId)
-
-    if(toggle && nav){
-        toggle.addEventListener("click" , ()=>{
-            nav.classList.toggle("show")
-        })
-    }
-}
-showMenu("navbarNav")
-
 
 console.log("executing main.js")
-$(document).Ready(function () {
-    
+$(document).ready(function () {
+
+    let $btns = $(".project-area .button-group button");
+
 
     $btns.click(function (e) {
+
+
         $(".project-area .button-group button").removeClass("active");
         e.target.classList.add("active");
 
@@ -27,11 +19,44 @@ $(document).Ready(function () {
         return false;
     })
 
-});
 
 
-$(".project-area .grid .test-popup-link").magnificPopup({
+    $("project-area .button-group #btn 1").trigger("click");
+
+    $(".project-area .grid .test-popup-link").magnificPopup({
     type: 'image',
     gallery:{enabled:true}
-    
   });
+  const showMenu = (toggled, navId) =>{
+    const toggle = document(toggleId),
+    nav = document.getElementById(navId)
+
+        if(toggle && nav){
+        toggle.addEventListener("click" , ()=>{
+            nav.classList.toggle("show")
+            })
+        }
+    }
+    showMenu("navbarNav")
+
+    // sticky navbar menu
+
+    let nav_offset_top = $(".header_area").height() + 50;
+
+    function navbarFixed() {
+    if($(".header_area").length){
+        $(window).scroll(function () {
+            let scroll = $(window).scrollTop();
+            if(scroll >= nav_offset_top){
+                $(".header_area .main-menu").addClass("navbar_fixed");
+            }else{
+                $(".header_area .main-menu").removeClass("navbar_fixed");
+            }
+            
+            
+        })
+    }
+    
+}
+    navbarFixed();
+});
